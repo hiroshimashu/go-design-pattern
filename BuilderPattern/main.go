390245017239271c6fc88f141181458c515d4639
hiroshimashu <hiroshimashu@gmail.com>
2020-10-10 20:51:27 +0900

    add: implemented main.go and makefile

--- BuilderPattern/main.go
+++ BuilderPattern/main.go
@@ -1,91 +1,13 @@
 package main
 
-/*
-* Director
- */
-
-type Dealer struct {
-	VehicleBuilder VehicleBuilder
-}
-
-func (d *Dealer) construct(parameters interface{}) {
-	d.VehicleBuilder.Build(parameters)
-}
-
-func NewDealer(vb VehicleBuilder) *Dealer {
-	d := &Dealer{
-		VehicleBuilder: vb,
-	}
-	return d
-}
-
-/*
-* Builder
- */
-
-type VehicleBuilder interface {
-	Build(parameters interface{})
-}
-
-/*
-* ConcreateBuilder
- */
-
-type Car struct {
-	color       string
-	vehicleType string
-	numOfWheel  int
-}
-
-func NewCar() *Car {
-	return &Car{}
-}
-
-type CarBuilder struct {
-	*Car
-}
-
-func (cb CarBuilder) setColor(color string) {
-	cb.Car.color = color
-}
-
-func (cb CarBuilder) setType(vehicleType string) {
-	cb.Car.vehicleType = vehicleType
-}
-
-func (cb CarBuilder) setNumOfWheel(numOfWheel int) {
-	cb.Car.numOfWheel = numOfWheel
-}
-
-func (cb CarBuilder) Build(parameters interface{}) {
-
-}
-
-func (cb CarBuilder) getResult() *Car {
-	return cb.Car
-}
-
-func NewCarBuilder() CarBuilder {
-	nc := NewCar()
-	cb := CarBuilder{
-		Car: nc,
-	}
-	return cb
-}
-
-type AirPlane struct {
-	color       string
-	vehicleType string
-	numOfWheel  int
-	brand       string
-}
-
-func NewAirPlan() *AirPlane {
-	return &AirPlane{
-		brand: "boeing",
-	}
-}
+import (
+	"fmt"
+)
 
 func main() {
+	user1 := NewUserBuilder(0).SetName("mashu_matrix").SetGender("men").SetAge(29).Build()
+	user2 := NewUserBuilder(1).SetName("mai").SetGender("women").SetAge(27).Build()
 
+	fmt.Printf("user1: %v\n", user1)
+	fmt.Printf("user2: %v", user2)
 }
